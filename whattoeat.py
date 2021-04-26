33cfa219490a52c601f625e879c3e17f46802b5f
明见 <66513481+A-kirami@users.noreply.github.com>
2021-04-27 06:59:24 +0800

    调整触发关键字，回复文字跟随关键词中的时间，fix：#1

--- whattoeat.py
+++ whattoeat.py
@@ -5,11 +5,11 @@ from hoshino.util import DailyNumberLimiter
 import hoshino
 
 sv_help = '''
-[今天吃啥/今天吃什么/吃点啥/吃啥] 看看今天吃啥
+[今天吃什么] 看看今天吃啥
 '''.strip()
 
 sv = Service(
-    name = '今天吃啥',  #功能名
+    name = '今天吃什么',  #功能名
     use_priv = priv.NORMAL, #使用权限   
     manage_priv = priv.ADMIN, #管理权限
     visible = True, #可见性
@@ -18,10 +18,9 @@ sv = Service(
     help_ = sv_help #帮助说明
     )
 
-_day_limit = 3
+_day_limit = 5
 _lmt = DailyNumberLimiter(_day_limit)
 absPath = './hoshino/modules/whattoeat/'
-
 
 def get_foods():
     if os.path.exists(absPath + 'foods.json'):
@@ -35,19 +34,21 @@ def get_foods():
         hoshino.logger.error(f'目录下未找到食谱')
     keys = list(words.keys())
     key = random.choice(keys)
-
     return words[key]["name"]
 
-@sv.on_rex(r'^(今天|早上|中午|晚上|夜宵)吃(什么|啥|点啥)')
+@sv.on_rex(r'^(今天|[早中午晚][上饭餐午]|夜宵)吃(什么|啥|点啥)')
 async def net_ease_cloud_word(bot,ev:CQEvent):
     uid = ev.user_id
     if not _lmt.check(uid):
         return
     _lmt.increase(uid)
+    match = ev.match
+    time = match.group(1).strip()
     food_name = get_foods()
-    to_eat = f'今天去吃{food_name}吧~'
+    to_eat = f'{time}去吃{food_name}吧~'
     try:
-        to_eat += '\n' + R.img(f'foods/{food_name}.jpg').cqcode
+        foodimg = ' '.join(map(str, [R.img(f'foods/{food_name}.jpg').cqcode,]))
+        to_eat += f'\n{foodimg}'
     except Exception as e:
         hoshino.logger.error(f'读取食物图片时发生错误{type(e)}')
     await bot.send(ev, to_eat)
